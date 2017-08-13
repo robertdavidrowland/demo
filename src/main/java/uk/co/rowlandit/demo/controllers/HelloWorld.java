6daa52ca766bd60f37c3b274c32005dc13bb0f07
Robert Rowland <robertdavidrowland@gmail.com>
2017-08-13 13:54:13 +0100

    Initial commit

---
+++ src/main/java/uk/co/rowlandit/demo/controllers/HelloWorld.java
@@ -0,0 +1,13 @@
+package uk.co.rowlandit.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+}
